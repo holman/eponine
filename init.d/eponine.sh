0659cfdf3d5fc8e3a5698096c9b0d76720121f21
Zach Holman <zach@zachholman.com>
2016-02-20 14:49:55 -0800

    update script

--- init.d/eponine.sh
+++ init.d/eponine.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 #
-# Starts and stops eponine.
+# Starts and stops eponine. Toss this in your /etc/init.d.
 
 # The path to the eponine directory
 path=/home/pi/apps/eponine
 
+cd $path
+
 case "$1" in
   start)
-    ./$path/eponine
+    ./eponine &
     ;;
   stop)
-    killall eponine
+    sudo killall eponine
     ;;
   *)
     echo "Usage:  {start|stop}"
